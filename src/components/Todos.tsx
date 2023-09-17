99241d32b3091e14f74e53eece1c2d91e952abc6
unknown <antonyvidetta1986@gmail.com>
2023-09-17 22:18:50 +1200

    Add todos file

---
+++ src/components/Todos.tsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react"
+
+const Todos = () => {
+    return (
+        <ul>
+            <li>Learn react</li>
+            <li>Learn typescript</li>
+        </ul>
+        );
+};
+
+export default Todos;
